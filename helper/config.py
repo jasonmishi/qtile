93f0c0eff4753d05352d1903a0281a133827ef16
Jason Mishike <jasonmishi@gmail.com>
2022-11-17 22:06:18 +0530

    add from_file

--- helper/config.py
+++ helper/config.py
@@ -11,12 +11,16 @@ config_path = os.path.join(script_dir, rel_path)
 
 
 class Config:
-    def __init__(self):
-        with open(config_path, mode="rb") as fp:
+    def __init__(self, path=config_path):
+        with open(path, mode="rb") as fp:
             self._config = tomllib.load(fp)
         screenConfig = self._config.get("screens", None)
         if screenConfig and screenConfig.get("edids", False):
             self.__clean_edids()
+
+    @classmethod
+    def from_file(cls, file_path):
+        return cls(file_path)
 
     def get(self, key):
         return self._config.get(key, None)
